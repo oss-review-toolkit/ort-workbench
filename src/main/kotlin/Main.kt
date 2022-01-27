0678fe8fa92ba8464e6b729737ba0be5854c50a8
Sebastian Schuberth <sschuberth@gmail.com>
2022-01-27 09:16:32 +0100

    Set a main window icon
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package org.ossreviewtoolkit.workbench
 
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.useResource
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowState
@@ -10,7 +13,8 @@ fun main() = singleWindowApplication(
     title = "ORT Workbench",
     state = WindowState(
         size = DpSize(1440.dp, 810.dp)
-    )
+    ),
+    icon = BitmapPainter(useResource("app-icon/icon.png", ::loadImageBitmap))
 ) {
     App(rememberAppState())
 }
