19c9a43396bbd0eaff6287f5ce87cb27ab5c3ccf
Martin Nonnenmacher <martin.nonnenmacher@gmail.com>
2022-02-04 15:04:08 +0100

    Link: Use a clickable Text instead of a TextButton
    The default paddings of the clickable text are not desired in most
    places.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@gmail.com>

--- src/main/kotlin/util/Link.kt
+++ src/main/kotlin/util/Link.kt
@@ -1,21 +1,31 @@
 package org.ossreviewtoolkit.workbench.util
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+import org.ossreviewtoolkit.workbench.theme.LightGray
 
 @Composable
-fun Link(text: String, onClick: () -> Unit) {
-    TextButton(onClick) {
-        Text(text)
-    }
+fun Link(text: String, enabled: Boolean = true, onClick: () -> Unit) {
+    Text(
+        text = text,
+        modifier = Modifier.clickable { if (enabled) onClick() },
+        color = if (enabled) MaterialTheme.colors.primary else LightGray
+    )
 }
 
 @Composable
 @Preview
 private fun LinkPreview() {
     Preview {
-        Link("Text link") {}
+        Column {
+            Link("Enabled link") {}
+            Link("Disabled link", enabled = false) {}
+        }
     }
 }
