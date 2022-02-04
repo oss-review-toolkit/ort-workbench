4be5a850bd95c73854bb7262de8a154c05792399
Martin Nonnenmacher <martin.nonnenmacher@gmail.com>
2022-02-04 16:23:55 +0100

    WebLink: Use a different icon
    Use the "open_in_new" icon which is more commonly used for links that
    open in another application.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@gmail.com>

--- src/main/kotlin/util/WebLink.kt
+++ src/main/kotlin/util/WebLink.kt
@@ -2,9 +2,11 @@
 
 package org.ossreviewtoolkit.workbench.util
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.TooltipArea
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -13,24 +15,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WebLink(text: String, url: String) {
     TooltipArea(tooltip = { Tooltip(url) }) {
         Row(
             modifier = Modifier.clickable { openUrlInBrowser(url) },
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Icon(
-                painterResource(MaterialIcon.LINK.resource),
+                painterResource(MaterialIcon.OPEN_IN_NEW.resource),
                 contentDescription = "link",
                 tint = MaterialTheme.colors.primary
             )
-            Text(
-                text,
-                style = MaterialTheme.typography.caption,
-                color = MaterialTheme.colors.primary
-            )
+
+            Text(text, color = MaterialTheme.colors.primary)
         }
+    }
+}
+
+@Composable
+@Preview
+private fun WebLinkPreview() {
+    Preview {
+        WebLink("Web Link", "")
     }
 }
