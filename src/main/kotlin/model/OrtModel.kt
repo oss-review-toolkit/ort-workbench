d51de49ac34182770f5b022453b8b931a63a935f
Sebastian Schuberth <sschuberth@gmail.com>
2022-08-24 11:10:30 +0200

    OrtModel: Remove an unused import
    It is unclear why `detekt` does not report this.
    
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- src/main/kotlin/model/OrtModel.kt
+++ src/main/kotlin/model/OrtModel.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
