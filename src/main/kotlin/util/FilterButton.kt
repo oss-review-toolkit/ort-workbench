8e609d1e0f64ea0808ed67dc7795f41478b96b3d
Martin Nonnenmacher <martin.nonnenmacher@gmail.com>
2022-12-27 19:11:58 +0100

    FilterButton: Add a variant that takes `FilterData` as input
    Add a variant of the `FilterButton` composable that takes `FilterData`
    as input. This reduces the amount of code required to create the filter
    buttons in the `Vulnerabilities` screen.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@gmail.com>

--- src/main/kotlin/util/FilterButton.kt
+++ src/main/kotlin/util/FilterButton.kt
@@ -3,17 +3,25 @@ package org.ossreviewtoolkit.workbench.util
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+
+import org.ossreviewtoolkit.workbench.model.FilterData
 
 @Composable
 fun <T> FilterButton(
@@ -43,6 +51,43 @@ fun <T> FilterButton(
                     onFilterChange(item)
                 }) {
                     dropdownItem(item)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun <T> FilterButton(
+    data: FilterData<T>,
+    label: String,
+    onFilterChange: (T) -> Unit,
+    convert: (T) -> String = { it.toString() }
+) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    Box {
+        OutlinedButton(onClick = { expanded = !expanded }) {
+            if (data.selectedItem == null) {
+                Text(label, overflow = TextOverflow.Ellipsis)
+            } else {
+                Text(convert(data.selectedItem), overflow = TextOverflow.Ellipsis)
+            }
+        }
+
+        DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            data.options.forEach { item ->
+                DropdownMenuItem(onClick = {
+                    expanded = false
+                    onFilterChange(item)
+                }) {
+                    if (item == null) {
+                        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                            Text("No filter", fontStyle = FontStyle.Italic, overflow = TextOverflow.Ellipsis)
+                        }
+                    } else {
+                        Text(convert(item), overflow = TextOverflow.Ellipsis)
+                    }
                 }
             }
         }
