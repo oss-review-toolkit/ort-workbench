03f599f0ec77ed5eec9b150a872f109ffbb27d9d
Martin Nonnenmacher <martin.nonnenmacher@gmail.com>
2022-12-28 00:14:19 +0100

    FilterButton: Remove an unused implementation
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@gmail.com>

--- src/main/kotlin/util/FilterButton.kt
+++ src/main/kotlin/util/FilterButton.kt
@@ -1,8 +1,6 @@
 package org.ossreviewtoolkit.workbench.util
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
@@ -15,47 +13,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 
 import org.ossreviewtoolkit.workbench.model.FilterData
-
-@Composable
-fun <T> FilterButton(
-    selectedItem: T,
-    items: List<T>,
-    buttonWidth: Dp = 130.dp,
-    dropdownWidth: Dp = 130.dp,
-    onFilterChange: (T) -> Unit,
-    buttonContent: @Composable RowScope.(selectedItem: T) -> Unit,
-    dropdownItem: @Composable RowScope.(item: T) -> Unit
-) {
-    var expanded by rememberSaveable { mutableStateOf(false) }
-
-    Box {
-        OutlinedButton(onClick = { expanded = !expanded }, modifier = Modifier.width(buttonWidth)) {
-            buttonContent(selectedItem)
-        }
-
-        DropdownMenu(
-            expanded = expanded,
-            onDismissRequest = { expanded = false },
-            modifier = Modifier.width(dropdownWidth)
-        ) {
-            items.forEach { item ->
-                DropdownMenuItem(onClick = {
-                    expanded = false
-                    onFilterChange(item)
-                }) {
-                    dropdownItem(item)
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun <T> FilterButton(
