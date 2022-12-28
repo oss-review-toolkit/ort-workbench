b49325d3b4541f81af83e7a298e1a092c11927c0
Martin Nonnenmacher <martin.nonnenmacher@gmail.com>
2022-12-28 14:31:15 +0100

    FilterTextField: Fix icon and placeholder colors
    The `FilterTextField` appears on the `primary` background so it should
    use the `onPrimary` color.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@gmail.com>

--- src/main/kotlin/util/FilterTextField.kt
+++ src/main/kotlin/util/FilterTextField.kt
@@ -3,7 +3,9 @@ package org.ossreviewtoolkit.workbench.util
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
@@ -32,7 +34,10 @@ fun FilterTextField(
         shape = RoundedCornerShape(10.dp),
         colors = TextFieldDefaults.textFieldColors(
             focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent
+            unfocusedIndicatorColor = Color.Transparent,
+            textColor = MaterialTheme.colors.onPrimary.copy(ContentAlpha.high),
+            leadingIconColor = MaterialTheme.colors.onPrimary.copy(ContentAlpha.medium),
+            placeholderColor = MaterialTheme.colors.onPrimary.copy(ContentAlpha.medium)
         ),
         modifier = modifier.widthIn(max = 300.dp)
     )
