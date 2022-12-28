8748573242d184b55d5194409920a9b48675a1da
Martin Nonnenmacher <martin.nonnenmacher@gmail.com>
2022-12-28 22:25:19 +0100

    Add a `ScreenAppBar` composable
    Add a `ScreenAppBar` composable to reduce code duplication.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@gmail.com>

---
+++ src/main/kotlin/composables/ScreenAppBar.kt
@@ -0,0 +1,18 @@
+package org.ossreviewtoolkit.workbench.composables
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.zIndex
+
+@Composable
+fun ScreenAppBar(title: @Composable () -> Unit, actions: @Composable RowScope.() -> Unit = {}) {
+    TopAppBar(
+        modifier = Modifier.zIndex(1f),
+        backgroundColor = MaterialTheme.colors.primary,
+        title = title,
+        actions = actions
+    )
+}
