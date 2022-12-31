72fc9c688a3a877a2e8223ed4c2d140c56754128
Martin Nonnenmacher <martin.nonnenmacher@gmail.com>
2022-12-31 14:09:28 +0100

    Add a basic NavigationController
    Add a navigation controller as preparation for supporting navigation
    with stacks and parameters.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@gmail.com>

---
+++ src/main/kotlin/navigation/NavigationController.kt
@@ -0,0 +1,36 @@
+package org.ossreviewtoolkit.workbench.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class NavigationController<SCREEN : Screen>(initialScreen: SCREEN? = null) {
+    private val stack = ArrayDeque<SCREEN>()
+
+    private val _currentScreen = MutableStateFlow<SCREEN?>(null)
+    val currentScreen: StateFlow<SCREEN?> get() = _currentScreen
+
+    init {
+        if (initialScreen != null) push(initialScreen)
+    }
+
+    fun push(screen: SCREEN) {
+        stack.addLast(screen)
+        _currentScreen.value = peek()
+    }
+
+    fun replace(screen: SCREEN) {
+        stack.clear()
+        push(screen)
+    }
+
+    fun pop(): SCREEN? = stack.removeLastOrNull().also { _currentScreen.value = peek() }
+
+    fun peek(): SCREEN? = stack.lastOrNull()
+}
+
+@Composable
+fun <SCREEN : Screen> rememberNavigationController(initialScreen: SCREEN? = null) =
+    remember { NavigationController(initialScreen) }
