e070d29e6abb060f977b0bad80c7965bd2285fe1
Martin Nonnenmacher <martin.nonnenmacher@gmail.com>
2023-02-23 23:14:03 +0100

    feat(PackageDetails): Add tabs for metadata and scan results
    Add a tab row with tabs for package metadata and scan results.
    
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@gmail.com>

--- src/main/kotlin/ui/packagedetails/PackageDetails.kt
+++ src/main/kotlin/ui/packagedetails/PackageDetails.kt
@@ -1,11 +1,18 @@
 package org.ossreviewtoolkit.workbench.ui.packagedetails
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Tab
+import androidx.compose.material.TabRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -14,18 +21,54 @@ import org.ossreviewtoolkit.model.Identifier
 import org.ossreviewtoolkit.workbench.composables.ScreenAppBar
 import org.ossreviewtoolkit.workbench.utils.MaterialIcon
 
+private const val TAB_METADATA = 0
+private const val TAB_SCAN_RESULTS = 1
+
 @Composable
 fun PackageDetails(id: Identifier, onBack: () -> Unit) {
-    ScreenAppBar(
-        title = { Text(id.toCoordinates(), style = MaterialTheme.typography.h3) },
-        navigationIcon = {
-            IconButton(onClick = onBack) {
-                Icon(
-                    painterResource(MaterialIcon.ARROW_BACK.resource),
-                    contentDescription = "Back",
-                    modifier = Modifier.size(32.dp)
-                )
+    Column {
+        ScreenAppBar(
+            title = { Text(id.toCoordinates(), style = MaterialTheme.typography.h3) },
+            navigationIcon = {
+                IconButton(onClick = onBack) {
+                    Icon(
+                        painterResource(MaterialIcon.ARROW_BACK.resource),
+                        contentDescription = "Back",
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
             }
+        )
+
+        var tabIndex by remember { mutableStateOf(TAB_METADATA) }
+
+        TabRow(selectedTabIndex = tabIndex) {
+            Tab(
+                text = { Text("Metadata") },
+                selected = tabIndex == TAB_METADATA,
+                onClick = { tabIndex = TAB_METADATA }
+            )
+
+            Tab(
+                text = { Text("Scan Results") },
+                selected = tabIndex == TAB_SCAN_RESULTS,
+                onClick = { tabIndex = TAB_SCAN_RESULTS }
+            )
         }
-    )
+
+        when (tabIndex) {
+            TAB_METADATA -> PackageMetadata()
+            TAB_SCAN_RESULTS -> PackageScanResults()
+        }
+    }
+}
+
+@Composable
+fun PackageMetadata() {
+    Text("Metadata")
+}
+
+@Composable
+fun PackageScanResults() {
+    Text("Scan Results")
 }
