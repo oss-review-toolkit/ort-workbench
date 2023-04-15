991184d0ec023ea7cb272d414962fc1deb31e933
Martin Nonnenmacher <martin.nonnenmacher@gmail.com>
2023-04-15 16:42:44 +0200

    refactor(Navigation): Use a delegate for the screen state
    Signed-off-by: Martin Nonnenmacher <martin.nonnenmacher@gmail.com>

--- src/main/kotlin/navigation/Navigation.kt
+++ src/main/kotlin/navigation/Navigation.kt
@@ -2,12 +2,13 @@ package org.ossreviewtoolkit.workbench.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 
 @Composable
 fun <SCREEN : Screen> Navigation(
     navigationController: NavigationController<SCREEN>,
     content: @Composable (screen: SCREEN?) -> Unit
 ) {
-    val currentScreen = navigationController.currentScreen.collectAsState()
-    content(currentScreen.value)
+    val currentScreen by navigationController.currentScreen.collectAsState()
+    content(currentScreen)
 }
