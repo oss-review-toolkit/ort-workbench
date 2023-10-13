73a82a5f8c27e4771a7915abe070e2d274cab84c
Sebastian Schuberth <sschuberth@gmail.com>
2023-10-13 14:44:24 +0200

    fix(OrtIcon): Use the correct advisor icon
    Signed-off-by: Sebastian Schuberth <sschuberth@gmail.com>

--- src/main/kotlin/utils/OrtIcon.kt
+++ src/main/kotlin/utils/OrtIcon.kt
@@ -1,7 +1,7 @@
 package org.ossreviewtoolkit.workbench.utils
 
 enum class OrtIcon(val resource: String) {
-    ADVISOR("analyzer.svg"),
+    ADVISOR("advisor.svg"),
     ANALYZER("analyzer.svg"),
     EVALUATOR("evaluator.svg"),
     SCANNER("scanner.svg")
